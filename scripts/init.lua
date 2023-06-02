4d388dd7a08f0db7f0d9ec0bb378d03f60a946da
Cyb3R <niklasm.cyber@gmail.com>
2023-06-02 15:41:12 +0200

    Detailed variant
    + fix wrong rule on sliding minigame sock in Goo Lagoon

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,8 +1,7 @@
 ENABLE_DEBUG_LOG = true
--- get current variant
 local variant = Tracker.ActiveVariantUID
--- check variant info
 IS_ITEMS_ONLY = variant:find("itemsonly")
+IS_DETAILED = variant:find("detailed")
 
 print("-- BfBB AP Tracker --")
 print("Loaded variant: ", variant)
@@ -21,9 +20,26 @@ Tracker:AddItems("items/items.json")
 
 if not IS_ITEMS_ONLY then
     -- Maps
-    Tracker:AddMaps("maps/maps.json")    
+    Tracker:AddMaps("maps/maps.json")
+    if IS_DETAILED then
+        Tracker:AddMaps(("maps/maps_detailed.json"))
+    end
     -- Locations
-    Tracker:AddLocations("locations/locations.json")
+    if IS_DETAILED then
+        Tracker:AddLocations("locations/hb.json")
+        Tracker:AddLocations("locations/jf.json")
+        Tracker:AddLocations("locations/bb.json")
+        Tracker:AddLocations("locations/gl.json")
+        Tracker:AddLocations("locations/bc.json")
+        Tracker:AddLocations("locations/rb.json")
+        Tracker:AddLocations("locations/sm.json")
+        Tracker:AddLocations("locations/kf.json")
+        Tracker:AddLocations("locations/gy.json")
+        Tracker:AddLocations("locations/db.json")
+    else
+        Tracker:AddLocations("locations/locations.json")
+    end
+
 end
 
 -- Layout
